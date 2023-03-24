a4a464bf1945079854f5fccbea3d2ae24bb9758c
Observer <sxpd7788+g@gmail.com>
2023-03-24 11:40:42 +0800

    添加注释

--- src/TagFacade.php
+++ src/TagFacade.php
@@ -7,7 +7,8 @@ use Illuminate\Support\Facades\Facade;
 /**
  * Class TagFacade
  *
- *  @method static searchTags(array $searchArr, array $orderArr, int $page = 1, int $prePage = 20)
+ *  @method static searchTags(array $searchArr, array $orderArr, int $page = 1, int $perPage = 20)
+ *  @method static searchTagGroups(array $searchArr, array $orderArr, int $page = 1, int $perPage = 20)
  */
 class TagFacade extends Facade
 {
