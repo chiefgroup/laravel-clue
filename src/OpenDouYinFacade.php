72199c9959376ebdc6880fbb063797ea81de82e9
erpeng <2512422541@qq.com>
2023-12-21 16:37:48 +0800

    fix OpenDouYinService method.

--- src/OpenDouYinFacade.php
+++ src/OpenDouYinFacade.php
@@ -7,8 +7,11 @@ use Illuminate\Support\Facades\Facade;
  * Class OpenDouYinFacade
  *
  * @method static mixed request(string $method, string $path, array $option)
- * @method static string getAccessToken(bool $force = false)
- * @method static mixed getAccessTokenByCode(string $code, bool $force = false)
+ * @method static string getClientToken(bool $force = false)
+ * @method static mixed getAccessTokenByCode(string $code, string $identity, bool $isToken = false)
+ * @method static mixed requestOther(string $method, string $path, array $option = [], ?string $identity = null)
+ * @method static mixed refreshToken(string $identity, ?string $refreshToken = null, ?string $clientKey = null)
+ * @method static mixed getAccessTokenByIdentity(string $identity, bool $isToken = false)
  * @method static mixed getConfig(string $option = null)
  */
 class OpenDouYinFacade extends Facade
